b1caac6ee0c5168176428ee0437dea886b6f049c
Rushikesh-Kate <Rushikesh.kate@revealhealthtech.com>
2023-09-11 12:29:48 +0530

    add xlsx and react-native-fs

--- src/screens/Dashboard.tsx
+++ src/screens/Dashboard.tsx
@@ -1,6 +1,14 @@
 import {useNavigation} from '@react-navigation/native';
 import React from 'react';
-import {Pressable, ScrollView, StyleSheet, Text, View} from 'react-native';
+import {
+  Button,
+  PermissionsAndroid,
+  Pressable,
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
 import {AppSVGs} from '../assets';
 import {HomeCard} from '../components';
 import AppContainer from '../components/common/AppContainer';
@@ -9,9 +17,15 @@ import {DashboardStackProps} from '../navigation/AppNavigation';
 import {authStore} from '../stores';
 import {colors, typography} from '../theme';
 import AppStrings from '../utils/AppStrings';
+var RNFS = require('react-native-fs');
+import XLSX from 'xlsx';
 
 const Dashboard = () => {
   const navigation = useNavigation<DashboardStackProps>();
+
+  PermissionsAndroid.request(
+    PermissionsAndroid.PERMISSIONS.WRITE_EXTERNAL_STORAGE,
+  );
 
   const homeCards = [
     {
@@ -55,6 +69,73 @@ const Dashboard = () => {
   };
   const auth = authStore;
 
+  const exportDataToExcel = () => {
+    // Created Sample data
+    let sample_data_to_export = [
+      {id: '1', name: 'First User'},
+      {id: '2', name: 'Second User'},
+    ];
+
+    let wb = XLSX.utils.book_new();
+    let ws = XLSX.utils.json_to_sheet(sample_data_to_export);
+    XLSX.utils.book_append_sheet(wb, ws, 'Users');
+    const wbout = XLSX.write(wb, {type: 'binary', bookType: 'xlsx'});
+
+    // Write generated excel to Storage
+    RNFS.writeFile(
+      RNFS.ExternalStorageDirectoryPath + '/my_exported_file.xlsx',
+      wbout,
+      'ascii',
+    )
+      .then(r => {
+        console.log('Success');
+      })
+      .catch(e => {
+        console.log('Error', e);
+      });
+  };
+
+  // const handleClick = async () => {
+  //   try {
+  //     // Check for Permission (check if permission is already given or not)
+  //     let isPermitedExternalStorage = await PermissionsAndroid.check(
+  //       PermissionsAndroid.PERMISSIONS.WRITE_EXTERNAL_STORAGE,
+  //     );
+
+  //     if (!isPermitedExternalStorage) {
+  //       console.log('permision not granted');
+  //       // Ask for permission
+  //       const granted = await PermissionsAndroid.request(
+  //         PermissionsAndroid.PERMISSIONS.WRITE_EXTERNAL_STORAGE,
+  //         {
+  //           title: 'Storage permission needed',
+  //           buttonNeutral: 'Ask Me Later',
+  //           buttonNegative: 'Cancel',
+  //           buttonPositive: 'OK',
+  //           message: 'test',
+  //         },
+  //       );
+  //       console.log(granted, 'granted');
+
+  //       if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+  //         // Permission Granted (calling our exportDataToExcel function)
+  //         exportDataToExcel();
+  //         console.log('Permission granted');
+  //       } else {
+  //         // Permission denied
+  //         console.log('Permission denied');
+  //       }
+  //     } else {
+  //       // Already have Permission (calling our exportDataToExcel function)
+  //       exportDataToExcel();
+  //     }
+  //   } catch (e) {
+  //     console.log('Error while checking permission');
+  //     console.log(e);
+  //     return;
+  //   }
+  // };
+
   return (
     <AppContainer style={styles.container}>
       <View style={styles.topContainer}>
@@ -66,6 +147,7 @@ const Dashboard = () => {
           <AppSVGs.profile style={styles.userLogo} />
           <View style={styles.welcomeContainer}>
             <Text style={styles.welcome}>Welcome,</Text>
+            <Button title="generate report" onPress={exportDataToExcel} />
             <ObservableChild>
               <Text style={styles.userName}>{authStore.userData.name}</Text>
             </ObservableChild>
