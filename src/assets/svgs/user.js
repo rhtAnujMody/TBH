755042a0aaf7c9c2c3b8b19792d026db4e9b4a31
Shashank <Teja.shashank@revealhealthtech.com>
2023-10-09 15:28:06 +0530

    Change Manage Users Icon

--- src/assets/svgs/user.js
+++ src/assets/svgs/user.js
@@ -1,6 +1,6 @@
-import * as React from 'react';
-import Svg, {Defs, G, Path} from 'react-native-svg';
-const SVGComponent = props => (
+import * as React from "react";
+import Svg, { Defs, G, Circle, Path } from "react-native-svg";
+const SVGComponent = (props) => (
   <Svg
     xmlns="http://www.w3.org/2000/svg"
     xmlnsXlink="http://www.w3.org/1999/xlink"
@@ -8,32 +8,51 @@ const SVGComponent = props => (
     height={30}
     viewBox="0 0 256 256"
     xmlSpace="preserve"
-    {...props}>
+    {...props}
+  >
     <Defs />
     <G
       style={{
-        stroke: 'none',
+        stroke: "none",
         strokeWidth: 0,
-        strokeDasharray: 'none',
-        strokeLinecap: 'butt',
-        strokeLinejoin: 'miter',
+        strokeDasharray: "none",
+        strokeLinecap: "butt",
+        strokeLinejoin: "miter",
         strokeMiterlimit: 10,
-        fill: 'none',
-        fillRule: 'nonzero',
+        fill: "none",
+        fillRule: "nonzero",
         opacity: 1,
       }}
-      transform="translate(1.4065934065934016 1.4065934065934016) scale(2.81 2.81)">
-      <Path
-        d="M 79.635 73.696 C 86.104 65.901 90 55.898 90 45 C 90 20.187 69.813 0 45 0 C 20.187 0 0 20.187 0 45 c 0 10.898 3.896 20.901 10.365 28.696 c 0.105 0.161 0.227 0.315 0.383 0.445 c 0.002 0.002 0.005 0.003 0.007 0.005 C 19.015 83.837 31.298 90 45 90 c 13.702 0 25.985 -6.163 34.245 -15.854 c 0.003 -0.002 0.005 -0.003 0.008 -0.005 C 79.408 74.01 79.53 73.857 79.635 73.696 z M 45 4 c 22.607 0 41 18.393 41 41 c 0 9.169 -3.026 17.645 -8.132 24.482 c -6.081 -6.505 -13.876 -10.99 -22.402 -13.023 c 6.497 -3.669 10.901 -10.629 10.901 -18.609 c 0 -11.782 -9.585 -21.367 -21.367 -21.367 c -11.782 0 -21.367 9.585 -21.367 21.367 c 0 7.979 4.404 14.939 10.901 18.608 c -8.526 2.033 -16.321 6.518 -22.402 13.023 C 7.026 62.645 4 54.169 4 45 C 4 22.393 22.393 4 45 4 z M 45 55.217 c -9.576 0 -17.367 -7.791 -17.367 -17.367 S 35.424 20.482 45 20.482 s 17.367 7.791 17.367 17.367 S 54.576 55.217 45 55.217 z M 45 86 c -11.986 0 -22.787 -5.171 -30.29 -13.399 C 22.48 64.079 33.418 59.217 45 59.217 c 11.581 0 22.52 4.863 30.29 13.384 C 67.787 80.829 56.986 86 45 86 z"
+      transform="translate(1.4065934065934016 1.4065934065934016) scale(2.81 2.81)"
+    >
+      <Circle
+        cx={45}
+        cy={45}
+        r={45}
         style={{
-          stroke: 'none',
+          stroke: "none",
           strokeWidth: 1,
-          strokeDasharray: 'none',
-          strokeLinecap: 'butt',
-          strokeLinejoin: 'miter',
+          strokeDasharray: "none",
+          strokeLinecap: "butt",
+          strokeLinejoin: "miter",
           strokeMiterlimit: 10,
-          fill: 'rgb(249,173,55)',
-          fillRule: 'nonzero',
+          fill: "rgb(249,173,55)",
+          fillRule: "nonzero",
+          opacity: 1,
+        }}
+        transform="  matrix(1 0 0 1 0 0) "
+      />
+      <Path
+        d="M 53.026 45.823 c 3.572 -2.527 5.916 -6.682 5.916 -11.381 C 58.941 26.754 52.688 20.5 45 20.5 s -13.942 6.254 -13.942 13.942 c 0 4.699 2.344 8.854 5.916 11.381 C 28.172 49.092 21.883 57.575 21.883 67.5 c 0 1.104 0.896 2 2 2 s 2 -0.896 2 -2 c 0 -10.541 8.576 -19.116 19.117 -19.116 S 64.116 56.959 64.116 67.5 c 0 1.104 0.896 2 2 2 s 2 -0.896 2 -2 C 68.116 57.575 61.827 49.092 53.026 45.823 z M 35.058 34.442 c 0 -5.482 4.46 -9.942 9.942 -9.942 c 5.481 0 9.941 4.46 9.941 9.942 s -4.46 9.942 -9.941 9.942 C 39.518 44.384 35.058 39.924 35.058 34.442 z"
+        style={{
+          stroke: "none",
+          strokeWidth: 1,
+          strokeDasharray: "none",
+          strokeLinecap: "butt",
+          strokeLinejoin: "miter",
+          strokeMiterlimit: 10,
+          fill: "rgb(255,255,255)",
+          fillRule: "nonzero",
           opacity: 1,
         }}
         transform=" matrix(1 0 0 1 0 0) "
